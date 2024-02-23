13daf31b2c34289ab80f9409fb1a8b7f6f84a4e6
AKASH ANURAGI <136264753+akdeveloper01@users.noreply.github.com>
2024-02-23 23:21:44 +0530

    Add files via upload

---
+++ 1input.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    /*int a,b;
+    cout<<"Enter the value of a :";
+    cin>>a;
+    cout<<"Enter the value of b :";
+    cin>>b;
+    cout<<"sum of number is : "<<a+b;
+    return(0);*/
+    int a=67;
+    char c='Z';
+    c= a;
+    cout<<c; //this the called type casting
+    return 0;
+}
