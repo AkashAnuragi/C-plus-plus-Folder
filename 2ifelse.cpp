776ce53a9516b24b5ff8867547bcb547cb6605af
AKASH ANURAGI <136264753+akdeveloper01@users.noreply.github.com>
2024-02-23 23:22:20 +0530

    2

---
+++ 2ifelse.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int age ;
+    cout<< "Enter the age :";
+    cin>>age;
+    if (age>=18 ){
+        cout<< "he/she is adult";
+    }
+    else{
+        cout<<"he/she is child";
+    }
+}
