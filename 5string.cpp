4a113e4c5a5e534caffc7d522858d8dda562c471
AKASH ANURAGI <136264753+akdeveloper01@users.noreply.github.com>
2024-02-24 21:16:22 +0530

    5

---
+++ 5string.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+    string fullname;
+    cout<<"Enter your full name : ";
+    getline(cin,fullname);
+    cout<<"Your name is : "<<fullname;
+    getch();
+}
