3d7cfe9f6a4bda4f1889c6ddaaebeb575bd41a39
AKASH ANURAGI <136264753+akdeveloper01@users.noreply.github.com>
2024-03-04 08:42:26 +0530

    14

---
+++ 1oddeven.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+int a;
+cout<<"Enter a number : " ;
+cin>>a;
+if (a%2==0){
+    cout<<"This number is a even number";
+}
+else{
+    cout<<"this number is a odd number.";
+}
+}
