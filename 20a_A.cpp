03b01e8f25933811d5885fea50b8e1abcdd99324
AKASH ANURAGI <136264753+akdeveloper01@users.noreply.github.com>
2024-03-14 23:51:26 +0530

    32

---
+++ 20a_A.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+char convert(char name)
+{
+    char ans =name -'a'+'A';
+    return ans;
+}
+
+int main()
+{
+    char name;
+    cin>>name;
+    cout<<convert(name);
+    return(0);
+}
