355c245928bf87420b937e13d6c306495caf4414
AKASH ANURAGI <136264753+akdeveloper01@users.noreply.github.com>
2024-03-16 23:30:14 +0530

    36

---
+++ 24searcharray.cpp
@@ -0,0 +1,24 @@
+//Search Element
+
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+int main()
+{
+   int arr[] ={23,43,234,12,21};
+//   int n,i;
+//   cout<<"Enter the searching number: ";
+//   cin>>n;
+   for(int i=4;i>=0;i--)
+   {
+
+
+           cout<<i<<" = "<<arr[i]<<endl;
+
+
+
+   }
+
+getch();
+}
